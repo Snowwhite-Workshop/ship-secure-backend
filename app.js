522c336d91ff9493d29c8f664dc24a62d2d79160
mumtazbukuru <70026185+mumtazbukuru@users.noreply.github.com>
2022-02-22 20:43:49 +0000

    create the base files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const mysql2 = require('mysql2');
+const cors = require('cors')
+
+// Allow frontend to connect with backend
+app.use(cors());
+app.use(express.json()); 
+
+// Mysql credential
+const db = mysql2.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'employeeSystem'
+})
+
+app.listen(5000, () => {
+    console.log("Server is running")
+})
